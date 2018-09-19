93c88dda9b25da7b6a5b783c921bf774f06ac873
Enzo Gladiadis <30397502+Tr0j4n9@users.noreply.github.com>
2018-09-20 11:37:36 +1200

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -111,10 +111,10 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0xfb;
-        pchMessageStart[1] = 0xc0;
-        pchMessageStart[2] = 0xb6;
-        pchMessageStart[3] = 0xdb;
+        pchMessageStart[0] = 0x1c;
+        pchMessageStart[1] = 0xc9;
+        pchMessageStart[2] = 0x7f;
+        pchMessageStart[3] = 0xb6;
         nDefaultPort = 9333;
         nPruneAfterHeight = 100000;
 
